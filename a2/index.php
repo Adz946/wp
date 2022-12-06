7967fb4c0f8f83a1e711553f83b9e09050bc86c8
Adz946 <100402347+Adz946@users.noreply.github.com>
2022-12-06 21:21:28 +1100

    A2 Update | 3
    Attempting to add a custom nav and footer

--- a2/index.php
+++ a2/index.php
@@ -10,6 +10,33 @@
 
   <body>
     <header>
-      <img src="../../media/myLogo.gif" height="128" alt="Lunardo Logo" />
+      <img src="../../media/myLogo.gif" height="128" alt="Lunardo Logo">Lunardo Cinemas</img>
+      <hr>
     </header>
+
+    <nav>
+      <div id="assignment_selection">
+        <button id="selector">Select Assignment</button>
+
+        <div id="content">
+          <a href="a1/index.html">A1 Page</a>
+          <a id="active" href="a2/index.php">A2 Home</a>
+          <a href="a2/booking.php">A2 Booking</a>
+        </div>
+      </div>
+    </nav>
+
+    <body>
+      <h1>Some Text</h1>
+    </body>
+
+    <footer>
+      <hr>
+
+      <p id="footer"> 
+        &copy; Adam Saleh, s3940024 | 
+        <script> document.write(new Date().getFullYear()); </script> |
+        <a target="_blank" href="https://github.com/Adz946/WebProgramming_SP4_2022">My Repo</a>
+      </p>
+  </footer>
 </html>
